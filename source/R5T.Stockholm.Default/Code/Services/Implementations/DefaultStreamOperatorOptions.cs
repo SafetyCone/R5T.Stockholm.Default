6fb086b2a7e8c4d891a548eee83f1102ed364baf
MinexAutomation <minex.automation@gmail.com>
2020-01-24 15:22:52 -0800

    Added IStreamOperatorOptions<T> implementation DefaultStreamOperatorOptions<T>.

---
+++ source/R5T.Stockholm.Default/Code/Services/Implementations/DefaultStreamOperatorOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace R5T.Stockholm.Default
+{
+    public class DefaultStreamOperatorOptions<T> : IStreamSerializerOptions<T>
+    {
+        public bool AddByteOrderMark { get; set; } = DefaultStreamOperatorOptions.DefaultAddByteOrderMark;
+    }
+
+
+    public class DefaultStreamOperatorOptions
+    {
+        public const bool DefaultAddByteOrderMark = true;
+    }
+}
